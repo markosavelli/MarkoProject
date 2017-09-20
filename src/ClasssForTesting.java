411b1d4be4f78467e263d79d0a4bff914c812c89
TA-15 <TA-15@172.30.12.32>
2017-09-20 15:14:21 +0300

    ClasssForTesting variable added

--- src/ClasssForTesting.java
+++ src/ClasssForTesting.java
@@ -3,9 +3,10 @@ public class ClasssForTesting {
 
 	public static void main(String[] args) {
 		String str = "Tere tulemast!";
-	    int number = 5;
+	    int number = 2;
 	    int number2 = 9;
-		System.out.println(str+number+number2);
+	    int number3 = 4;
+		System.out.println(str+number+number2+number3);
 	}
 
 }
