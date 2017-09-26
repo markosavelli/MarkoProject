7e56de12f24361472a0789e17a46822d577c0cd8
Mark <mark.maeste@gmail.com>
2017-09-26 13:49:34 +0300

    Print method added.

--- src/ClasssForTesting.java
+++ src/ClasssForTesting.java
@@ -7,6 +7,13 @@ public class ClasssForTesting {
 	    int number2 = 9;
 	    int number3 = 4;
 		System.out.println(str+number+number2+number3);
+		
+		printMethod();
+		 
+	}
+	
+	public static void printMethod() {
+		System.out.println("Tere, Marko");
 	}
 
 }
